dd0d89c7910b61ad36a6eb60c6b3a31c1124cec0
MysticUnios1998 <eduard.arias@mail.escuelaing.edu.co>
2021-01-27 09:13:45 -0500

    Modificado el método main para aceptar múltiples hilos de ejecución

--- parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -1,12 +1,36 @@
 package edu.eci.arsw.primefinder;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-		PrimeFinderThread pft=new PrimeFinderThread(0, 30000000);
+//		PrimeFinderThread pft=new PrimeFinderThread(0, 30000000);
+//		
+//		pft.start();
 		
-		pft.start();
+		int nThreads = 1;
+		if (nThreads <= 0 || nThreads > 30000001) nThreads = 1;
+		
+		int seccion = 30000001/nThreads;
+		if (30000001 % nThreads != 0) {nThreads++;}
+		List<PrimeFinderThread> threads = new ArrayList<PrimeFinderThread>(nThreads);
+		
+		for (int i=0; i<nThreads; i++) {
+			threads.add(i, new PrimeFinderThread(i*seccion, Math.min(30000000, (i+1)*seccion-1)));
+			threads.get(i).start();
+		}
+		
+		for (PrimeFinderThread t: threads) {
+			try {
+				t.join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 		
 		
 	}
